ca3c1ac40e8727a0b9e02edbf1a092096dd696bc
frank13russo <48385074+frank13russo@users.noreply.github.com>
2020-12-27 08:22:36 -0500

    hello world

---
+++ FoodTracker/FoodTracker/Shared/FoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTrackerApp.swift
+//  Shared
+//
+//  Created by Frank Russo on 27/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct FoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
